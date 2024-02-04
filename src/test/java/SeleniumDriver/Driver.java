bfc185866b8b12091da03d12650ce7c7e8a85ba2
Maxim Ryabtsev <maksim.riabtsev@unicardtech.ru>
2024-02-04 05:03:50 +0300

    New Cases; DeleteDoc, OpenDoc

--- src/test/java/SeleniumDriver/Driver.java
+++ src/test/java/SeleniumDriver/Driver.java
@@ -16,7 +16,8 @@ public class Driver {
 
     @BeforeMethod
     public static void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\macs0\\Desktop\\work\\WABP_Tests\\src\\test\\java\\SeleniumDriver\\chromedriver.exe");        //WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver", "src/test/java/SeleniumDriver/chromedriver.exe");
+        //WebDriverManager.chromedriver().setup();
         ChromeOptions ops = new ChromeOptions();
         ops.addArguments("--remote-allow-origins=*");
         ops.setCapability("unexpectedAlertBehaviour", "dismiss");
